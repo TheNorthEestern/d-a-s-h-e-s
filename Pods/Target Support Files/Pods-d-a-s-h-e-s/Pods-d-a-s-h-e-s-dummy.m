54acb0fbae04c809127232b884cdee6839820f1a
Kacy James <inquen@gmail.com>
2017-02-04 01:50:23 -0500

    Added MarqueeLabel

---
+++ Pods/Target Support Files/Pods-d-a-s-h-e-s/Pods-d-a-s-h-e-s-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_d_a_s_h_e_s : NSObject
+@end
+@implementation PodsDummy_Pods_d_a_s_h_e_s
+@end
---
+++ Pods/Target Support Files/Pods-d-a-s-h-e-s/Pods-d-a-s-h-e-s-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_d_a_s_h_e_sVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_d_a_s_h_e_sVersionString[];
+
