b266b496dfd6e6cb861537987adcf0c62aa89725
Kacy James <inquen@gmail.com>
2017-02-22 10:02:51 -0500

    Updated NSExtension prefs, containing app content

--- d-a-s-h-e-s/ViewController.swift
+++ d-a-s-h-e-s/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var bannerText: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        bannerText.charInterval = 0.5
-        bannerText.text = "hello"
+        bannerText.charInterval = 0.2
+        bannerText.text = "h-e-l-l-o!"
         // Thread.sleep(forTimeInterval: bannerText.charInterval * 5)
         // bannerText.text = StringManipulator.dashify(bannerText.text)
         // Do any additional setup after loading the view, typically from a nib.
